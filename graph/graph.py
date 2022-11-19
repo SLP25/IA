2513eca7d0d4d4904df73d3092276a82d04b5c04
Luís Pereira <54816048+lumafepe@users.noreply.github.com>
2022-11-19 18:21:44 +0000

    Revert "Tb/4dnodes"

--- graph/graph.py
+++ graph/graph.py
@@ -1,20 +1,26 @@
 from .node import Node
 
 class Graph:
-    def __init__(self, adjList = {}, nodes = set(),
-                 starts = [], finishes = [], directed = True):
-        self.adjList = {}
-        self.nodes = set()
-        self.directed = True
-        self.starts = []
-        self.finishes =  []
+    def __init__(self, adjList = {}, nodes = [], estimates = {}, 
+                 start = [], finish = [], directed = False):
+        self.adjList = adjList
+        self.nodes = nodes
+        self.directed = directed
+        self.estimates = estimates
+        self.start = start
+        self.finish = finish
 
     def add_edge(self, src_node, dest_node, cost):
-        if src_node not in self.nodes:
-            self.nodes.add(src_node)
+        n1 = Node(src_node)
+        n2 = Node(dest_node)
 
-        if dest_node not in self.nodes:
-            self.nodes.add(dest_node)
+        if n1 not in self.nodes:
+            n1.setId(len(self.nodes))
+            self.nodes.append(n1)
+
+        if n2 not in self.nodes:
+            n2.setId(len(self.nodes))
+            self.nodes.append(n2)
 
         if src_node not in self.adjList.keys():
             self.adjList[src_node] = set()
@@ -27,8 +33,11 @@ class Graph:
         if not self.directed:
             self.adjList[dest_node].add((src_node, cost))
 
-    def add_start(self, x, y):
-        self.starts.append((x,y))
+    def add_estimate(self, node, value):
+        self.estimates[node] = value
 
-    def add_finish(self, x, y):
-        self.finishes.append((x,y))
+    def add_start(self, node):
+        self.start.append(node)
+
+    def add_finish(self, node):
+        self.finish.append(node)
--- graph/node.py
+++ graph/node.py
@@ -1,47 +1,22 @@
-import math
-
 class Node:
-    def __init__(self, x, y, vx, vy):
-        self.x = x
-        self.y = y
-        self.vx = vx
-        self.vy = vy
-        self.__estimate__ = None
-        
-    def getEstimate(self, finishes):
-        if self.__estimate__ == None:
-            self.__estimate__ = self.__gen_estimate__(finishes)
-        return self.__estimate__
+    def __init__(self, name, id = -1):
+        self.id = id
+        self.name = name
+
+    def getId(self):
+        return self.id
+
+    def getName(self):
+        return self.name
+
+    def setId(self, id):
+        self.id = id
+
+    def setName(self, name):
+        self.name = name
 
     def __str__(self):
-        return f"Node: p=({self.x},{self.y}) v=({self.vx},{self.vy})"
+        return f"Node: {self.name}"
     
     def __eq__(self, node):
-        return isinstance(node,Node) and node.x == self.x and node.y == self.y and node.vx == self.vx and node.vy == self.vy
-    
-    def __hash__(self):
-        return hash((self.x, self.y, self.vx, self.vy))
-    
-    def __gen_estimate__(self, finishes):
-        return min(map(lambda f: max(__estimateAxis__(self.x,self.vx,f[0]),
-                                     __estimateAxis__(self.y,self.vy,f[1])), finishes))
-        
-    def deserialize(self):
-        return (self.x, self.y, self.vx, self.vy)
-
-def __estimateAxis__(i, vi, f):
-    if i == f:
-        return 0
-    elif i < f:
-        a = 0.5
-        b = vi + 0.5
-        c = i - f
-    else:
-        a = -0.5
-        b = vi - 0.5
-        c = i - f
-
-    S = [(-b - math.sqrt(b * b - 4 * a * c)) / (2 * a),
-            (-b + math.sqrt(b * b - 4 * a * c)) / (2 * a)]
-
-    return math.ceil(min(filter(lambda t: t >= 0, S)))
+        return node.name == self.name
